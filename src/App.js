f0dd179c8b31739818ec34bb794d8c58aa9e75e1
Justin Joco <justin.joco@gmail.com>
2024-05-25 13:54:08 -0700

    created simple calculator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+function evaluateExpression(total, operator, number){
+  let result = 0
+  switch (operator){
+    case "+":
+        result = total + number
+        break
+      case "-":
+        result = total - number
+        break
+      case "*":
+        result = total * number
+        break
+      case "/":
+        result = total / number
+        break
+  }
+  return result
+}
+// Clear display when a new operator is pressed and a new button is pressed
 function App() {
+  const [total, setTotal] = useState(0)
+  const [display, setDisplay] = useState("")
+  const [operator, setOperator] = useState("")
+
+  function handleOperatorButton(event){
+    event.preventDefault()
+    setOperator(event.target.value)
+   
+    switch (event.target.value){
+      case "+":
+        setOperator("+")        
+        setTotal(Number(display))
+        setDisplay("")
+        break
+      case "-":
+        setOperator("-")
+        setTotal(Number(display))
+        setDisplay("")
+        break
+      case "*":
+        setOperator("*")
+        setTotal(Number(display))
+        setDisplay("")
+        break
+      case "/":
+        setOperator("/")
+        setTotal(Number(display))
+        setDisplay("")
+        break
+      case "=":
+        let newTotal = evaluateExpression(total, operator, Number(display))
+        setTotal(newTotal)
+        setDisplay(newTotal)
+        break
+    }
+  }
+  
+  function handleNumberButton(event){
+    event.preventDefault()
+    setDisplay(`${display}${event.target.value}`)
+  }
+
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  const numberButtons = numbers.map((number)=> {
+    return <button id={`button${number}`} value={number} onClick={(e) => handleNumberButton(e)}>{number}</button>
+  })
+  const operators = ["+", "-", "/", "*", "="]
+  const operatorButtons = operators.map((operator)=> {
+    return <button id={`button${operator}`} value={operator}  onClick={(e) => handleOperatorButton(e)}>{operator}</button>
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea value={display}/>
+        {numberButtons}
+        {operatorButtons}
       </header>
     </div>
   );
