d9e20d865b52bdade2a56e222de8fae9f79496e9
Justin Joco <justin.joco@gmail.com>
2024-05-27 20:48:43 -0700

    changes file layout; ;

---
+++ basic/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Example</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
